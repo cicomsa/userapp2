233470ea9138f4ae2a2f1562f72fb8c5ab5a098a
cicomsa <ioanacomsa85@gmail.com>
2020-08-09 16:23:35 +0100

    homepage component added

---
+++ src/components/Homepage/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+const Homepage = () => {
+  return (
+    <Link to='/page-a'>Page A}</Link>
+  )
+}
+
+export default Homepage
