d74d27184d438d547f06ecad1129b5da290deb72
cicomsa <ioanacomsa85@gmail.com>
2020-08-09 16:36:10 +0100

    removed links - revert change

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -3,11 +3,7 @@ import { Link } from "react-router-dom";
 
 const Homepage = () => {
   return (
-    <>
-      <Link className="link" to='/page-a'>Page A</Link>
-      <Link className="link" to='/page-b'>Page B</Link>
-      <Link className="link" to='/page-c'>Page C</Link>
-    </>
+    <Link to='/page-a'>Page A</Link>
   )
 }
 
