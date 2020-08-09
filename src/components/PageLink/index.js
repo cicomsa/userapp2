733a1a451ab351fcc22fab085037f19db15ea4b0
cicomsa <ioanacomsa85@gmail.com>
2020-08-09 02:42:08 +0100

    removed ; - PageLink

--- src/components/PageLink/index.js
+++ src/components/PageLink/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom"
 
 const PageLink = ({ linkTitle, path }) =>  {
-  const history = useHistory();
+  const history = useHistory()
 
   const handleClick = () => {
     history.push(path);
@@ -12,7 +12,7 @@ const PageLink = ({ linkTitle, path }) =>  {
     <button type="button" onClick={handleClick}>
       {linkTitle}
     </button>
-  );
+  )
 }
 
 export default PageLink
