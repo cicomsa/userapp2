0afc210b3dd61fe4d7fbedb15dee34c03341dbe5
cicomsa <ioanacomsa85@gmail.com>
2020-08-09 16:42:21 +0100

    removed comment App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Content from './components/Content'
 
-// To implement a dynamic way to write the bellow Routes
 const App = () => {
   return (
     <Router>
