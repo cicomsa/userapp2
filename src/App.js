ab0ed389310f8694ad4c16172dd56a02749f4ad4
cicomsa <ioanacomsa85@gmail.com>
2020-08-09 17:00:29 +0100

    quotations marks change

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Content from './components/Content'
 
+// To implement a dynamic way to write the bellow Routes
 const App = () => {
   return (
     <Router>
