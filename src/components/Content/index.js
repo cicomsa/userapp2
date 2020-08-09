c8541cc2a2c4dad83005955eb69d40f043765445
cicomsa <ioanacomsa85@gmail.com>
2020-08-09 02:04:29 +0100

    edit comment

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -15,7 +15,7 @@ const Content = () => {
   const location = useLocation()
   const pathName = location.pathname
 
-  // data should come from an API - mock functionality not implemented
+  // data should come from an API call/GraphQL query - mock functionality not implemented
   const content = data.find(content => content.path === pathName)
   const contentKeys = Object.keys(content)
 
